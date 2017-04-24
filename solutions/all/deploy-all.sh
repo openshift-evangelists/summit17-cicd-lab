bfd015d5d9ba5e8581f8cd32d310e728f8d170c2
Siamak Sadeghianfar <ssadeghi@redhat.com>
2017-04-25 00:05:35 +0700

    upgraded gogs

--- solutions/all/deploy-all.sh
+++ solutions/all/deploy-all.sh
@@ -12,7 +12,7 @@ oc new-project prod --display-name="Coolstore Prod"
 oc process -f https://raw.githubusercontent.com/OpenShiftDemos/nexus/master/nexus2-template.yaml | oc create -f - -n lab-infra
 oc set resources dc/nexus --limits=cpu=1,memory=2Gi --requests=cpu=200m,memory=1Gi -n lab-infra
 
-oc process -f https://raw.githubusercontent.com/OpenShiftDemos/gogs-openshift-docker/master/openshift/gogs-persistent-template.yaml --param=SKIP_TLS_VERIFY=true --param=HOSTNAME=gogs-lab-infra.$HOSTNAME_SUFFIX --param=GOGS_VERSION=0.9.113 -n lab-infra | oc create -f - -n lab-infra
+oc process -f https://raw.githubusercontent.com/OpenShiftDemos/gogs-openshift-docker/master/openshift/gogs-persistent-template.yaml --param=SKIP_TLS_VERIFY=true --param=HOSTNAME=gogs-lab-infra.$HOSTNAME_SUFFIX --param=GOGS_VERSION=0.11.4 -n lab-infra | oc create -f - -n lab-infra
 
 oc process -f $BASE_DIR/../lab-2/cart-template.yaml --param=GIT_URI=$GIT_URI --param=MAVEN_MIRROR_URL= | oc create -f - -n dev
 oc process -f $BASE_DIR/../lab-7/coolstore-bluegreen-template.yaml --param=HOSTNAME_SUFFIX=prod.$HOSTNAME_SUFFIX | oc create -f - -n prod
